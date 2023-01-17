ef94fbb73460f6c9c0c545d178fc72e684830fe4
Shubham_kale2 <kshubh00200@gmail.com>
2023-01-17 15:35:23 +0000

    1st day contaner sized widts

---
+++ lib/widgets/container_sized.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Container_Sized extends StatelessWidget {
+  const Container_Sized({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text('Container And sized box'),
+      ),
+    );
+  }
+}
